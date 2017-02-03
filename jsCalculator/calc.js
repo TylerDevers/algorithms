35681d0763b5a1542c3ceca5d665d2e9f4f85164
TylerDevers <devers.tyler@gmail.com>
2017-02-02 20:19:00 -0800

    revamped code and styled with minimal css. Looks decent but now calc buttons do not work

--- jsCalculator/calc.js
+++ jsCalculator/calc.js
@@ -1,8 +1,11 @@
-function calcDisplay(num) {
-    calc.screen.value = calc.screen.value + num;
+
+function calculatorDisplay(num) {
+    //calculator.screen.value = calculator.screen.value + num;
+    var answer = document.getElementById('display').value;
+    answer = answer += num;
 }
 
 function crunchNumbers() {
-    var result = eval(calc.screen.value);
-    calc.screen.value = result;
+    var result = eval(calculator.screen.value);
+    calculator.screen.value = result;
 }
