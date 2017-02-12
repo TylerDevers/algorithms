ae80c9694218b801eedc925545e56fafc5082dc1
TylerDevers <devers.tyler@gmail.com>
2017-02-12 12:55:15 -0800

    fixed timer bug. Timer no longer fails to work after third click. Now working on break timer incorporation

--- pomodoroClock/ext.js
+++ pomodoroClock/ext.js
@@ -1,24 +1,37 @@
-var controls = true;
-var now = new Date().getTime();
-var count = setInterval(countdown, 1000);
+/*
+ * to-do:
+ * include break time into timer, so that it starts as soon as session  
+ * time is up.
+ * 
+ */
 
+var controls = true;
+//var now = new Date().getTime();
+var count; // will hold setInterval() once timer is triggered
+
+//helper function for setInterval
 function countdown() {
-	var time = document.getElementById('session-time').innerHTML;
-	if (time > 0){
+	var sessionTime = document.getElementById('session-time').innerHTML;
+	var breakTime = document.getElementById('break-time').innerHTML;
+	if (sessionTime > 0){
 		document.getElementById('session-time').innerHTML --;
-	} else if (time === 0) {
+		console.log('countdown');
+	} else if (sessionTime === 0) {
 		clearInterval(count);
 	}
 }
 
+//triggers when session timer is clicked
 function timer() {
 	//disable controls when session timer is clicked
 	if (controls === true) {
 		controls = false;
+		count = setInterval(countdown, 1000);
 		count;
 	} else {
 			controls = true;
 			clearInterval(count);
+			document.getElementById('session-time').innerHTML = document.getElementById('session-length').innerHTML;
 	}
 	
 }
